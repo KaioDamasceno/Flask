8798c401116ebe1f70b4d9f767e283f0fb37bd74
KaioDamasceno <132790635+KaioDamasceno@users.noreply.github.com>
2024-08-02 09:07:11 -0300

    Add files via upload

---
+++ mvc_flask/mvc_flask/models.py
@@ -0,0 +1,7 @@
+class Tarefa:
+    def __init__(self, id, descricao, concluido=False):
+        self.id = id
+        self.descricao = descricao
+        self.concluido = concluido
+        
+tarefas = []
