545fbd286f2ab4efcb739e14f2d453eff2f33cb3
KaioDamasceno <132790635+KaioDamasceno@users.noreply.github.com>
2024-11-07 22:59:29 -0300

    Delete mvc_flask directory

--- mvc_flask/mvc_flask/controllers.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-from .models import tarefas
-
-tarefa_controller = Blueprint('tarefa', __name__)
-
-@tarefa_controller.route('/')
-def index():
-    return render_template('index.html', tarefas=tarefas)
--- mvc_flask/mvc_flask/models.py
+++
@@ -1,7 +0,0 @@
-class Tarefa:
-    def __init__(self, id, descricao, concluido=False):
-        self.id = id
-        self.descricao = descricao
-        self.concluido = concluido
-        
-tarefas = []
